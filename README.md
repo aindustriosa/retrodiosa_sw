fa606451a2923220c16b4c834bde8374f1b08417
hmightypirate <hcerezoc@gmail.com>
2018-12-21 20:35:50 +0100

    Initial commit with sw

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# retrodiosa_sw
-Software relacionado con el proyecto Retrodiosa: una recreativa para el local de A Industriosa
+# Software of Retrodiosa
+
+## Introduction
+
+This script install additional scripts and themes for the retrodiosa arcade machine, a flamboyant arcade made in A Industriosa.
+
+## Features
+
+- Custom themes for A Industriosa
+- Integration of games made with LIBGDX (playable directly from  emulation station). This only works for non-arm architectures
+- HTTP server for uploading games to the arcade machine
+
+More detailed instructions (Spanish) can be found in https://github.com/aindustriosa/retrodiosa_docs/blob/master/modificaciones_retropie.md
+
+
+## Installation
+
+1) Install retroach following the instructions https://retropie.org.uk/docs/Debian/
+
+2) Execute the script
+
+```
+./install_script.sh
+```
+
+## Known Issues
+
+Currently, only x64 architectures are supported.
+
+Installation script only tested in debian-like distributions
+
---
+++ install_script.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+RETROPIE_ROMS="RetroPie/roms"
+RETROPIE_INSTALL="/opt/retropie"
+TEST=".test"
+
+
+# FIXME delete
+function install_systems() {
+    ## copy the file with the systems (we are going to create a new one)
+
+    $SUDO cp /etc/emulationstation/es_systems.cfg $RETROPIE_INSTALL/configs/all/emulationstation/es_systems.cfg$TEST
+
+    LAST_SYSTEM=$(cat $RETROPIE_INSTALL/configs/all/emulationstation/es_systems.cfg$TEST | grep -n "</system>" | cut -f1 -d: | tail -n 1)
+    echo "$LAST_SYSTEM"
+    
+    AINDUSTRIOSA_SYSTEMS=" \
+  <system>\n \
+    <name>aindustriosa</name>\n \
+    <fullname>A industriosa Games</fullname>\n \
+    <path>$ROMPATH/aindustriosa</path>\n \
+    <extension>.7z .nes .zip .7Z .NES .ZIP</extension>\n \
+    <command>$RETROPIE_INSTALL/supplementary/runcommand/runcommand.sh 0 _SYS_ nes %ROM%</command>\n \
+    <platform>favs</platform>\n \
+    <theme>aindustriosa</theme>\n \
+  </system>\n \
+\n \
+  <system>\n \
+    <name>aindustriosalibgdx</name>\n \
+    <fullname>A industriosa Games for LIBGDX</fullname>\n \
+    <path>$ROMPATH/aindustriosa</path>\n \
+    <extension>.sh</extension>\n \
+    <command>%ROM%</command>\n \
+    <platform>favs</platform>\n \
+    <theme>aindustriosa_libgdx</theme>\n \
+  </system>\n"
+
+    $SUDO sed -i "$LAST_SYSTEM a $AINDUSTRIOSA_SYSTEMS" $RETROPIE_INSTALL/configs/all/emulationstation/es_systems.cfg$TEST
+
+    # copy the themes if they dont exist
+    if [[ ! -e $RETROPIE_INSTALL/configs/all/emulationstation/themes ]]; then
+	$SUDO cp -r /etc/emulationstation/themes $RETROPIE_INSTALL/configs/all/emulationstation/themes
+
+    else
+	echo "Folder (themes) already exists"
+    fi
+
+    # copy the aindustriosa themes to the themes folder
+    MYPATH=$(dirname "$0")
+
+    $SUDO cp -r $MYPATH/themes/* /etc/emulationstation/themes $RETROPIE_INSTALL/configs/all/emulationstation/themes/
+    
+}
+
+SUDO=''
+
+# check if we are running as root
+if [ $EUID -ne 0 ]; then
+    # user is not root, running with sudo
+    SUDO="sudo"
+fi
+
+function install_env() {
+    $SUDO apt-get install virtualenv openjdk8-jdk python-dev python3-http-parser
+    MYPATH=$(dirname "$0")
+
+    # using a virtual environment
+    virtualenv -p python2 venv
+
+    $MYPATH/venv/bip/pip --upgrade pip
+    
+    $MYPATH/venv/bin/pip install pyautogui pyudev xlib psutil
+}
+
+function install_scripts() {
+
+    MYPATH=$(dirname "$0")
+
+    $SUDO chmod +x $MYPATH/scripts/run_libgdx_game.sh
+    $SUDO chmod +x $MYPATH/scripts/joy2libgdxkey.py
+
+    MYREALPATH=$(realpath $MYPATH)
+        
+    $SUDO cp $MYPATH/scripts/* $RETROPIE_INSTALL/supplementary/runcommand/
+
+    $SUDO sed -i "1 s~^.*$~#! $MYREALPATH/venv/bin/python~" $RETROPIE_INSTALL/supplementary/runcommand/joy2libgdxkey.py
+    
+}
+
+echo "$SUDO"
+
+# find the path to retropie roms
+ROMPATH=$(find ~ -name "roms" | grep -FzZ $RETROPIE_ROMS)     
+
+echo "$ROMPATH"
+
+# create the folder to store aindustriosa roms
+if [[ ! -e $ROMPATH/aindustriosa ]]; then
+    mkdir $ROMPATH/aindustriosa
+
+else
+    echo "Folder already exists"
+fi
+
+# add the aindustriosa systems after the last original one
+install_systems
+install_env
+install_scripts
+
+#TODO install server
+
